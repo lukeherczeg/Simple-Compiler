3ec487f4aa5ff21fe76c7b7a6e23cd7a08190e73
lukeherczeg <lukeherczeg@ufl.edu>
2018-03-24 18:00:16 -0400

    Classified

--- src/pa3.cpp
+++ src/pa3.cpp
@@ -6,10 +6,47 @@
 #include "pa3.h"
 
 
+void Reader::read(std::ifstream file){
+	std::string word;
+	while(file >> word){
+		this->wordFrequency[word]++;
+	}
+	std::cout << std::endl;
+	for(this->it = this->wordFrequency.begin(); this->it != this->wordFrequency.end(); (this->it)++){
+		std::cout << this->it->first << " frequency: " << this->it->second << std::endl;
+	}
+	file.close();
+}
 
 
-void addWord(std::string input, std::vector<std::string> & wordList, std::vector<int> & frequency){
-	for(int i = 0; i < wordList.size(); i++){
+int main(){
+	Reader * r = new Reader();
+	std::string fileLocation;
+	std::ifstream file(fileLocation.c_str());
+	std::cin >> fileLocation;
+
+	r->read(file);
+
+	return 0;
+}
+
+
+
+
+
+/* Parallel Vectors
+std::vector <std::string> wordList;
+std::vector <int> frequency;
+while(file >> input){
+	addWord(input,wordList,frequency);
+}
+for(int i = 0; i < wordList.size(); i++){
+	std::cout << "Word: " << wordList[i] << "   Frequency: " << frequency[i] << std::endl;
+}*/
+
+
+/*void addWord(std::string input, std::vector<std::string> & wordList, std::vector<int> & frequency){
+	for(unsigned int i = 0; i < wordList.size(); i++){
 		if(input == wordList[i]){
 			frequency[i]++;
 			return;
@@ -17,54 +54,4 @@ void addWord(std::string input, std::vector<std::string> & wordList, std::vector
 	}
 	wordList.push_back(input);
 	frequency.push_back(1);
-}
-
-
-
-int main(){
-
-	std::string fileLocation;
-	std::cin >> fileLocation;
-	std::ifstream file(fileLocation.c_str());
-	std::string word;
-
-
-	/* Parallel Vectors
-	std::vector <std::string> wordList;
-	std::vector <int> frequency;
-	while(file >> input){
-		addWord(input,wordList,frequency);
-	}
-	for(int i = 0; i < wordList.size(); i++){
-		std::cout << "Word: " << wordList[i] << "   Frequency: " << frequency[i] << std::endl;
-	}
-
-	*/
-	//Hash Map
-	/*Important things for hashing functions
-	 * Completely random
-	 * Same input - same output
-	 * Fast
-	 *
-	 *
-	 * Use modulus to get index 0 - (tableSize-1)
-	 * (%tableSize)
-	 *
-	 * */
-
-	std::map<std::string, int> wordFrequency;
-	while(file>>word){
-		wordFrequency[word]++;
-	}
-
-	for(std::map<std::string, int>::iterator it = wordFrequency.begin(); it != wordFrequency.end(); ++it){
-		std::cout << it->first << " frequency: " << it->second << std::endl;
-	}
-
-	/*while(getline(file, input)){
-		std::cout << input << std::endl;
-	}*/
-	file.close();
-
-	return 0;
-}
+}*/
--- src/pa3.h
+++ src/pa3.h
@@ -1,7 +1,13 @@
 #ifndef PA3_H_
 #define PA3_H_
 
-
+class Reader{
+private:
+	std::map<std::string, int> wordFrequency;
+public:
+	std::map<std::string, int>::iterator it;
+	void read(std::ifstream file);
+};
 
 
 
